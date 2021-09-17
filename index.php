c5eb0c8da88e7e343ee8117a33f56356aefb6be8
mhelmyfaiz <mochfhelmy@gmail.com>
2021-09-17 23:55:30 +0700

    penambahan index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ class index extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Mochamad Helmy Faiz Saya tinggal di daerah Tanjung Priuk olah raga yang saya sukai adalah Bulutangkis";
+        echo "Nama saya Mochamad Helmy Faiz Saya tinggal di daerah Tanjung Priuk olah raga yang saya sukai adalah Membuat video dan Edit Video";
     }
 }
